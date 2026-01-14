bb9a0cd4d382f30d61a7b9307d267becc40ba602
Yashas Uttangi <yashuttangi@gmail.com>
2026-01-13 22:51:02 -0500

    Adding setup and requirements.txt

---
+++ setup.py
@@ -0,0 +1,27 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as f:
+    long_description = f.read()
+
+__version__ = "0.0.0"
+
+REPO_NAME = "Cancer-classification-end-to-end-using-MLFlow-and-DVC"
+AUTHOR_USER_NAME = "yashasuttangi"
+SRC_REPO = "ChestCancerClassifer"
+AUTHOR_EMAIL = "yashuttangi@gmail.com"
+
+setuptools.setup(
+    name=SRC_REPO,
+    version=__version__,
+    author=AUTHOR_USER_NAME,
+    author_email=AUTHOR_EMAIL,
+    description="A small package for cancer classification using MLFlow and DVC",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url=f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}",
+    project_urls={
+        "Bug Tracker": f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}/issues",
+    },
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src")
+)
