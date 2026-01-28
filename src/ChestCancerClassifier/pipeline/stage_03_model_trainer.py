75b9926dbd46d0b47d640c75ea019a7d34d792f6
Yashas Uttangi <yashuttangi@gmail.com>
2026-01-28 00:46:58 -0500

    Model trainer component

---
+++ src/ChestCancerClassifier/pipeline/stage_03_model_trainer.py
@@ -0,0 +1,28 @@
+from ChestCancerClassifier.config.configuration import ConfigurationManager
+from ChestCancerClassifier.components.model_trainer import Training
+from ChestCancerClassifier import logger 
+
+STAGE_NAME = "Model Training Stage"
+
+class ModelTrainingPipeline:
+    def __init__(self):
+        pass
+
+    def main(self):
+        config = ConfigurationManager()
+        training_config = config.get_training_config()
+        training = Training(config = training_config)
+        training.get_base_model()
+        training.train_valid_generator()
+        training.train()
+
+    
+if __name__ == "__main__":
+    try:
+        logger.info(">>>>>>>>>> {STAGE_NAME} started <<<<<<<<<<")
+        obj = ModelTrainingPipeline()
+        obj.main()
+        logger.info(">>>>>>>>>> {STAGE_NAME} completed <<<<<<<<<<\n\nx")
+    except Exception as e:
+        logger.exception(e)
+        raise e
