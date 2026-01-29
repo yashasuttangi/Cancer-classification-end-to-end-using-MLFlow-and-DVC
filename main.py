95412ce25438f4ac91de6fb09d49bc673bfcee87
Yashas Uttangi <yashuttangi@gmail.com>
2026-01-29 18:07:25 -0500

    updating main.py to direct the flow of the proj stage by stage.

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from ChestCancerClassifier import logger 
 from ChestCancerClassifier.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
-
+from ChestCancerClassifier.pipeline.stage_02_prepare_base_model import PrepareBaseModelTrainingPipeline
+from ChestCancerClassifier.pipeline.stage_03_model_trainer import ModelTrainingPipeline
+from ChestCancerClassifier.pipeline.stage_04_model_evaluation import EvaluationPipeline
 
 STAGE_NAME = "Data Ingestion Stage"
 
@@ -12,3 +14,37 @@ try:
 except Exception as e:
     logger.exception(e)
     raise e
+
+STAGE_NAME = "Prepare Base Model"
+
+try:
+    logger.info(f"*****************")
+    logger.info(f">>>>>>> stage {STAGE_NAME} started <<<<<<<<< ")
+    prepare_base_model = PrepareBaseModelTrainingPipeline()
+    prepare_base_model.main()
+    logger.info(f">>>>>>>> stage {STAGE_NAME} completed <<<<<<<< \n \nx ==========x")
+except Exception as e:
+    logger.exception(e)
+    raise e 
+
+STAGE_NAME = "Training"
+try:
+    logger.info(f"*****************")
+    logger.info(f">>>>>>>> stage {STAGE_NAME} started <<<<<<<")
+    model_evaluation = ModelTrainingPipeline()
+    model_evaluation.main()
+    logger.info(f">>>>>>>> stage {STAGE_NAME} completed <<<<<<<")
+except Exception as e:
+    logger.exception(e)
+    raise e 
+
+STAGE_NAME = "Evaluation Stage"
+try:
+    logger.info(f"******************")
+    logger.info(f">>>>>>>> stage {STAGE_NAME} started <<<<<<<<<")
+    model_evaluation = EvaluationPipeline()
+    model_evaluation.main()
+    logger.info(f">>>>>>>>> stage {STAGE_NAME} completed <<<<<<<\n\nx========x")
+except Exception as e:
+    logger.exception(e)
+    raise e 
